ffeec1b1d91a7cd44842d9c4c881ab0d9dfd701f
Jenn Le <jenn.thakugan@icloud.com>
2016-10-15 15:17:37 -0500

    Added some heart rate stuff

--- ImageLab/HeartRateDetectionViewController.swift
+++ ImageLab/HeartRateDetectionViewController.swift
@@ -11,4 +11,72 @@ import AVFoundation
 
 class HeartRateDetectionViewController: UIViewController{
     
+    //MARK: Class Properties
+    var filters : [CIFilter]! = nil
+    var videoManager:VideoAnalgesic! = nil
+    let pinchFilterIndex = 2
+    var detector:CIDetector! = nil
+    let bridge = OpenCVBridgeSub()
+    
+    
+    //MARK: ViewController Hierarchy
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = nil
+        
+        self.videoManager = VideoAnalgesic.sharedInstance
+        
+        //We're only using the back camera for this
+        self.videoManager.setCameraPosition(AVCaptureDevicePosition.back)
+        
+        self.videoManager.setProcessingBlock(self.processImage)
+        
+        if !videoManager.isRunning{
+            videoManager.start()
+        }
+        
+    }
+    
+    //MARK: Process image output
+    func processImage(_ inputImage:CIImage) -> CIImage{
+        
+        var retImage = inputImage
+        
+        // if you just want to process on separate queue use this code
+        // this is a NON BLOCKING CALL, but any changes to the image in OpenCV cannot be displayed real time
+        //        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) { () -> Void in
+        //            self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
+        //            self.bridge.processImage()
+        //        }
+        
+        // use this code if you are using OpenCV and want to overwrite the displayed image via OpenCv
+        // this is a BLOCKING CALL
+        //        self.bridge.setTransforms(self.videoManager.transform)
+        //        self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
+        //        self.bridge.processImage()
+        //        retImage = self.bridge.getImage()
+
+        
+        self.bridge.processImage()
+        retImage = self.bridge.getImageComposite() // get back opencv processed part of the image (overlayed on original)
+        
+        return retImage
+    }
+    
+    @IBAction func toggle_camera(_ sender: AnyObject) {
+        videoManager.toggleCameraPosition()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        if(self.videoManager.isRunning){
+            self.videoManager.turnOffFlash()
+            self.videoManager.stop()
+            self.videoManager.shutdown()
+        }
+    }
+    
+    @IBAction func toggleFlash(_ sender: AnyObject) {
+        self.videoManager.toggleFlash()
+    }
+    
 }
