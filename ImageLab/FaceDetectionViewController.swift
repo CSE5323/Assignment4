d6827061ad785b23b55a5c9d458f0d44873313a7
Jenn Le <jenn.thakugan@icloud.com>
2016-10-15 14:38:25 -0500

    Updated view controllers

---
+++ ImageLab/FaceDetectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FaceDetectionViewController.swift
+//  ImageLab
+//
+//  Created by Jenn Le on 10/15/16.
+//  Copyright © 2016 Eric Larson. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class FaceDetectionViewController: UIViewController{
+    
+}
